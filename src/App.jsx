ec9914f282650002d8ce0c96487feb0ee3a70174
Ololade <adesuyiololade@gmail.com>
2025-08-17 21:11:17 +0100

    home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
 import Timetable from "./pages/Timetable";
-import TestApi from "./pages/TestApi";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/globals.css";
 
@@ -11,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/timetable" element={<Timetable />} />
-        <Route path="/test-api" element={<TestApi />} />
+    
 
       </Routes>
     </Router>
